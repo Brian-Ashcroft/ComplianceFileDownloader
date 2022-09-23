5d046118d8f98f6bea4e05941516f9b6bae27102
AzureAD\BrianAshcroft <Brian.Ashcroft@ayahealthcare.com>
2022-09-23 17:48:57 -0400

    adds counter to ensure correct doc count

--- ComplianceFileDownloader/Downloaders/AclsDownloader.cs
+++ ComplianceFileDownloader/Downloaders/AclsDownloader.cs
@@ -27,8 +27,11 @@ namespace ComplianceFileDownloader.Downloaders
             csv.AppendLine("DocumentTypeId, CandidateDocumentId, DocumentId, Status, Reason, FirstName, LastName, ExpirationDate, FacilityDescription, AssociationDescription");
 
             var aclsId = 13;
-            var queries = new List<string>();
-            queries.Add(@"SELECT distinct top 10
+
+            int fileCount;
+            var queries = new List<queryDto>();
+            queries.Add(new queryDto(750,
+                         @"SELECT distinct top 2000
                             r.CandidateDocumentId
                             ,DocumentTypeId
                             ,d.Id DocumentId
@@ -51,9 +54,10 @@ namespace ComplianceFileDownloader.Downloaders
 		                    LEFT JOIN [nurses].dbo.hospQMrequire adesc on adesc.associationid = fp.qmsystemcode AND adesc.docid = @DocumentTypeId
                             WHERE DocumentTypeId = @DocumentTypeId
                             AND ValidationStatusId = 2
-                            order by r.id desc");
+                            order by r.id desc"));
 
-            queries.Add(@"SELECT distinct TOP 5 
+            queries.Add(new queryDto(125,
+                        @"SELECT distinct TOP 500 
                             r.CandidateDocumentId
                             ,r.DocumentTypeId
                             ,d.Id DocumentId
@@ -76,9 +80,10 @@ namespace ComplianceFileDownloader.Downloaders
 		                    LEFT JOIN [nurses].dbo.facilityprofiles fp on fp.id = ci.hospid
 		                    LEFT JOIN [nurses].dbo.hospQMrequire adesc on adesc.associationid = fp.qmsystemcode AND adesc.docid = @DocumentTypeId
                             WHERE r.DocumentTypeId = @DocumentTypeId
-                            ORDER BY rdn.Id desc");
+                            ORDER BY rdn.Id desc"));
 
-            queries.Add(@"SELECT distinct TOP 5 
+            queries.Add(new queryDto(125,
+                        @"SELECT distinct TOP 500 
                             cd.Id CandidateDocumentId
                             ,crp.DocumentTypeId
                             ,d.Id DocumentId
@@ -103,17 +108,21 @@ namespace ComplianceFileDownloader.Downloaders
 			                LEFT JOIN nurses.dbo.hospQMrequire adesc on adesc.associationid = fp.qmsystemcode AND adesc.docid = @DocumentTypeId
                             WHERE crp.DocumentTypeId = @DocumentTypeId
                             and cdn.TypeId = 1
-                            ORDER BY cdn.Id desc");
+                            ORDER BY cdn.Id desc"));
 
             using var connection = new SqlConnection(connectionString);
             var parameters = new { DocumentTypeId = aclsId };
-            foreach (var sql in queries)
+            foreach (var query in queries)
             {
-                var query = await connection.QueryAsync<AclsDoc>(sql, parameters);
-                var documents = query.ToList();
+                var result = await connection.QueryAsync<AclsDoc>(query.Sql, parameters);
+                var documents = result.ToList();
                 var token = await HttpRequestFactory.GetApiToken(userName, password, tokenUrl);
+                fileCount = 0;
+
                 foreach (var document in documents)
                 {
+                    if(fileCount >= query.Count) break;
+
                     if (File.Exists($"acls_docs/{document.DocumentId}.pdf"))
                     {
                         csv.AppendLine($"{document.DocumentTypeId}," +
@@ -150,6 +159,7 @@ namespace ComplianceFileDownloader.Downloaders
                             Directory.CreateDirectory("acls_docs");
                             using var fs = new FileStream($"acls_docs/{document.DocumentId}.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
                             await docResult.Content.CopyToAsync(fs);
+                            fileCount++;
                         }
                         else
                         {
@@ -161,6 +171,9 @@ namespace ComplianceFileDownloader.Downloaders
                         Console.WriteLine(document.DocumentId + " " + ex.ToString());
                     }
                 }
+
+
+
             }
             File.WriteAllText("acls_docs.csv", csv.ToString());
         }
---
+++ ComplianceFileDownloader/queryDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplianceFileDownloader
+{
+    public class queryDto
+    {
+        public int Count { get; }
+        public string Sql { get; }
+
+        public queryDto(int count, string sql)
+        {
+            Count = count;
+            Sql = sql;
+        }
+    }
+}
