a196767e23635cfdf3fb79be322cdde391a90aa3
Brian Ashcroft <Brian.Ashcroft@ayahealthcare.com>
2024-01-30 10:55:35 -0500

    Adding non OCR docs

---
+++ ComplianceFileDownloader/Downloaders/NonOCRDocDownloader.cs
@@ -0,0 +1,84 @@
+﻿using ComplianceFileDownloader.Entities;
+using ComplianceFileDownloader.Entities.Config;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace ComplianceFileDownloader.Downloaders
+{
+    internal class NonOCRDocDownloader
+    {
+		private readonly BlobStorageConfig _blobConfig;
+		private readonly int _docTypeId;
+        public NonOCRDocDownloader(BlobStorageConfig blobConfig, int documentTypeId)
+        {
+            _blobConfig = blobConfig;
+            _docTypeId = documentTypeId;
+        }
+        public async Task Execute()
+        {
+            var tokenUrl = _blobConfig.BaseUrl + "connect/token";
+            var downloadDocUrl = _blobConfig.BaseUrl + "ayanova/documents/";
+
+            int fileCount;
+            var queries = new List<queryDto>
+            {
+                new queryDto(30,
+                    @"SELECT DISTINCT TOP 200 r.CandidateDocumentId
+                        ,r.DocumentTypeId
+                        ,d.Id DocumentId
+                        ,d.Path
+                    FROM nurses.Compliance.Requirements r
+                    JOIN nurses.Compliance.CandidateDocuments cd
+	                    ON r.CandidateDocumentId = cd.Id
+                    JOIN nurses.Documents.Documents d
+	                    ON cd.DocumentId = d.Id
+                    WHERE r.DocumentTypeId = @DocumentTypeId"),
+            };
+
+            using var connection = new SqlConnection(_blobConfig.ConnectionString);
+            var parameters = new { DocumentTypeId = _docTypeId };
+            foreach (var query in queries)
+            {
+                var result = await connection.QueryAsync<Doc>(query.Sql, parameters);
+                var documents = result.ToList();
+                var token = await HttpRequestFactory.GetApiToken(_blobConfig.UserName, _blobConfig.Password, tokenUrl);
+                fileCount = 0;
+
+                foreach (var document in documents)
+                {
+                    if(fileCount >= query.Count) break;
+
+                    if (File.Exists($"non_ocr_docs/DocType-{_docTypeId}-{document.DocumentId}.pdf"))
+                    {
+                        continue;
+                    }
+                    try
+                    {
+                        var request = new HttpRequestBuilder();
+                        request.AddBearerToken(token);
+                        request.AddMethod(HttpMethod.Get);
+                        request.AddRequestUri(downloadDocUrl + document.DocumentId);
+                        var docResult = await request.SendAsync();
+                        if (docResult.IsSuccessStatusCode)
+                        {
+                           
+                            Directory.CreateDirectory($"non_ocr_docs");
+                            using var fs = new FileStream($"non_ocr_docs/DocType-{_docTypeId}-{document.DocumentId}.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
+                            await docResult.Content.CopyToAsync(fs);
+                            fileCount++;
+                        }
+                        else
+                        {
+                            Console.WriteLine(document.DocumentId + " " + docResult.ReasonPhrase);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(document.DocumentId + " " + ex.ToString());
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
--- ComplianceFileDownloader/Program.cs
+++ ComplianceFileDownloader/Program.cs
@@ -12,7 +12,56 @@ var blobConfig = new BlobStorageConfig()
 	ConnectionString = ConfigurationSettings.AppSettings.Get("connectionString")
 };
 
-//BRIAN
+
+////var doctypes = new List<int>
+////{
+////	 1, 3, 4, 8, 9, 10, 11, 12, 15, 18,
+////	 19, 20, 22, 23, 24, 27, 31, 37, 40, 41,
+////	 42, 43, 44, 45, 48, 50, 51, 52, 53, 54,
+////	 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,
+////	 66, 67, 68, 69, 70, 71, 72, 73, 74, 75
+////};
+
+////var doctypes = new List<int>
+////{
+////	76, 77, 78 , 79, 80, 81, 83, 84, 85, 86,
+////	87, 88, 89, 90, 82, 93, 154, 214, 217, 219,
+////	225, 232, 233, 234, 256, 257, 277, 293, 294, 325,
+////	440, 532, 554, 592, 605, 606, 648, 649, 674, 739,
+////	925, 928, 939, 940, 941, 942, 943, 947, 972, 1043
+////};
+
+////var doctypes = new List<int>
+////{
+////	1855, 2074, 2194, 2205, 2252, 2275, 2294, 2327, 2331, 2332,
+////	2343, 2356, 2357, 2358, 2516, 2517, 2528, 2554, 2555, 2569,
+////	2789, 2887, 2947, 3010, 3054, 3055, 3170, 3296, 3371, 3377,
+////	3423, 3466, 3665, 4101, 4102, 4122, 4560, 5118, 5603, 5944,
+////	6140, 6204, 6205, 6206, 6207, 6208, 6736, 7117, 7513, 7514
+////};
+
+////var doctypes = new List<int>
+////{
+////	8559, 10454, 10481, 10594, 13868, 14642, 14676, 14763, 14998, 15002,
+////	16640, 18772, 18923, 18966, 18985, 18988, 18989, 18990, 18991, 19376,
+////	20168, 20512, 20513, 20953, 22249, 22554, 24002, 24206, 24415, 25382,
+////	25383, 25384, 25699, 27718, 31196, 31890, 32403, 32404, 32680, 32825,
+////	33449, 33450, 33451, 33452, 33453, 33454, 33455, 33456, 34240, 35145
+////};
+
+var doctypes = new List<int>
+{
+	35489, 37456, 41828, 57729, 58129, 59087, 59712, 60589, 62186, 62187,
+	62188, 62189, 62190, 62581
+};
+
+
+foreach (int i in doctypes)
+{
+    await new NonOCRDocDownloader(blobConfig, i).Execute();
+}
+
+
 
 //await new DocDownloader(blobConfig, new DocDownloadConfig(30, "NegTbOrChestXray", 100, 25, 25)).Execute();
 //await new DocDownloader(blobConfig, new DocDownloadConfig(35, "HepatitisB", 100, 25, 25)).Execute();
@@ -27,8 +76,6 @@ var blobConfig = new BlobStorageConfig()
 //await new DocDownloader(blobConfig, new DocDownloadConfig(605, "TranscriptOrDiploma", 100, 25, 25)).Execute();
 //await new DocDownloader(blobConfig, new DocDownloadConfig(2206, "NameChange", 100, 25, 25)).Execute();
 
-//NATHAN
-
 //await new DocDownloader(blobConfig, new DocDownloadConfig(972, "FingerPrinting", 100, 25, 25)).Execute();
 //await new DocDownloader(blobConfig, new DocDownloadConfig(52, "StateLicense", 100, 25, 25)).Execute();
 //await new DocDownloader(blobConfig, new DocDownloadConfig(26, "ElectronicFetalMonitoring", 100, 25, 25)).Execute();
@@ -41,3 +88,7 @@ var blobConfig = new BlobStorageConfig()
 //await new DocDownloader(blobConfig, new DocDownloadConfig(91, "I9SupportingDoc", 100, 25, 25)).Execute();
 //await new DocDownloader(blobConfig, new DocDownloadConfig(21, "CHEMO", 100, 25, 25)).Execute();
 //await new DocDownloader(blobConfig, new DocDownloadConfig(34240, "CovidExemptionReligious", 100, 25, 25)).Execute();
+
+
+
+
